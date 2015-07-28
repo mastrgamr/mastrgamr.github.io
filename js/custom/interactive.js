7d06007f3d45f447d833d0a350f82ded5a5dfdff
Stuart Smith <themastrgamr@gmail.com>
2015-07-27 23:34:59 -0400

    More work Less play
    Cleaned up some files, beginning serious dev...

--- js/custom/interactive.js
+++ js/custom/interactive.js
@@ -1,10 +1,5 @@
 var toggle = $('.toggle');
 
-var contactBtn = $('.contact');
-var about = $('#aboutContent');
-var portfolio = $('#portfolioContent');
-var swapContent = false;
-
 var button = $('.btn');
 
 var navHighlights = document.getElementsByClassName('navbar-nav');
@@ -14,13 +9,4 @@ toggle.click(function() {
 		toggle.removeClass('active');
 		$(this).addClass('active');
 	}
-	//alert("Clicked!");
-});
-
-button.click(function() {
-	$('.foot').addClass('fade');
-});
-
-$('.portfolio').click(function() {
-	aboutContent.fadeOut(400);
 });
