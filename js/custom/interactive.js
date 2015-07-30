093cd9cad1e07b259a785325574e631f8df2ea47
Stuart Smith <themastrgamr@gmail.com>
2015-07-29 23:44:10 -0400

    Needs More Pretty
    More Style updates, behind the scenes nav in the works.

--- js/custom/interactive.js
+++ js/custom/interactive.js
@@ -10,3 +10,35 @@ toggle.click(function() {
 		$(this).addClass('active');
 	}
 });
+
+$(window).scroll(function() {
+	if($('.navbar').offset().top > 5){
+		$('.navbar-fixed-top').addClass('animated').addClass('slideOutUp')
+		$('.nav-animation').removeClass('fadeOutLeft').addClass('fadeInLeft');
+	} else if ($('.navbar').offset().top < 5) {
+		$('.navbar-fixed-top').removeClass('slideOutUp').addClass('slideInDown');
+		$('.nav-animation').removeClass('fadeInLeft').addClass('fadeOutLeft');
+	}
+});
+
+$(document).ready(
+    function(){
+        $('#sidenav').mouseenter(
+            function(){
+                $(this)
+                    .animate(
+                        {
+                            'width':'150px'
+                        },250);
+            }
+        ).mouseleave(
+            function(){
+                $(this)
+                    .animate(
+                        {
+                            'width':'50px'
+                        },250);
+            }
+        );
+    }
+);
