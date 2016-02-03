0696a9df399fe9e034743eb2a303b62f6d86004a
mastrgamr <themastrgamr@gmail.com>
2016-02-02 23:12:59 -0500

    bug fixes

--- site.js
+++ site.js
@@ -1,13 +1,13 @@
 //Where my jQuery and Vanilla go.... mmmmm, Vanilla
-$(document).ready(function() {
-    
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', 'particles.json', function() {
-      console.log('callback - particles.js config loaded');
-    });
-	
-    //ignore the function below
-	$( '#play-btn' ).click(function() {  
+$(document).ready(function () {
+
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', 'particles.json', function () {
+		//console.log('callback - particles.js config loaded');
+	});
+
+	//ignore the function below
+	$('#play-btn').click(function () {
 		$(this).addClass('animated fadeOutRight');
 		/*
 		$(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
@@ -15,23 +15,19 @@ $(document).ready(function() {
 		});
 		*/
 	});
-	
+
 	//var navToTop = $('#nav').offset().top;
-    var navToTop = $('.header').height();
-    //var navHeight = $(nav).height(); //TODO: this isnt the fucking height!! 
-    //magic number = 85
-	
+	var headerHeight = $('.header').height();
+	//console.dir('$(\'.header\').height(): ' + $('.header').height());
+
 	$(window).scroll(function () {
-		
-	  //console.log($(window).scrollTop())
-		if ($(window).scrollTop() > navToTop) {
-		  console.log('At the Top!');
-			$('#nav').addClass('fixed-nav');
-            $('.content').css("padding-top", 137); //TODO: patchwork, idk were the extra 40px coming from
+
+		//console.log($(window).scrollTop())
+		if ($(window).scrollTop() > headerHeight) {
+			console.log('My head!');
+			$(nav).addClass('fixed-nav');
 		} else {
-            $('#nav').removeClass('fixed-nav');
-            $('.content').css("padding-top", 97);
-        }
-  });
-	
+			$(nav).removeClass('fixed-nav');
+		}
+	});
 });
