bae76274a1093e81efd7ceab5f37fe3b8124873d
mastrgamr <themastrgamr@gmail.com>
2016-02-11 21:32:08 -0500

    some styling and jQuery patchup

--- site.js
+++ site.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 	particlesJS.load('particles-js', 'particles.json', function () {
-		//console.log('callback - particles.js config loaded');
+		console.log('callback - particles.js config loaded');
 	});
 
 	//ignore the function below
@@ -26,8 +26,17 @@ $(document).ready(function () {
 		if ($(window).scrollTop() > headerHeight) {
 			console.log('My head!');
 			$(nav).addClass('fixed-nav');
+			$('.content').css('margin-top', headerHeight - 82);
+			$('.content').css('padding-top', 132);
 		} else {
 			$(nav).removeClass('fixed-nav');
+			$('.content').css('margin-top', headerHeight);
+			$('.content').css('padding-top', 0);
+		}
+		
+		if ($('.animated').scrollTop() > headerHeight + 500) {
+			console.log('OY');
+			$('.animated').addClass('slideInUp');
 		}
 	});
 });
